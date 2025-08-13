1da256f7989b6d8de6188b8c586a52f83c423eb9
davidjwhitfield <davidjwhitfield@gmail.com>
2025-08-13 11:45:04 +0100

    Clear branch for deployment output

--- global.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-var */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-declare var TextEncoder: typeof window.TextEncoder;
-declare var TextDecoder: typeof window.TextDecoder;
-export {};
