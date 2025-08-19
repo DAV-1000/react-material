d826252b7cad5b5b7ab3fbaea28e972f0f52456b
davidjwhitfield <davidjwhitfield@gmail.com>
2025-08-19 11:59:16 +0100

    Api calling CosmosDb working locally

--- api/src/functions/HttpExample.ts
+++ api/src/functions/HttpExample.ts
@@ -1,11 +1,26 @@
+import { CosmosClient } from "@azure/cosmos";
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 
-export async function HttpExample(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+export async function HttpExample(request: HttpRequest, context: InvocationContext): 
+Promise<HttpResponseInit> {
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const cosmosConnectionString = process.env.COSMOS_DB_CONNECTION_STRING;
 
-    return { body: `Hello, ${name}!` };
+    if (!cosmosConnectionString) {
+        throw new Error("COSMOS_DB_CONNECTION_STRING not set");
+    }
+
+    const client = new CosmosClient(cosmosConnectionString);
+
+    const database = client.database("cosmicworks");
+    const container = database.container("posts");
+
+    const { resources } = await container.items.query("SELECT * FROM c").fetchAll();
+
+    return {
+        status: 200,
+        jsonBody: resources
+    };
 };
 
 app.http('HttpExample', {
