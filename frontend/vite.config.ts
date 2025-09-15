cef1753d30d89a6ee9c960a00e1fefd6381a4be3
davidjwhitfield <davidjwhitfield@gmail.com>
2025-09-15 16:05:21 +0100

    Removed build chunking config from vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,18 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("@mui")) return "mui"; // separate chunk for MUI
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
   server: {
     proxy: {
       "/api": {
