75a27a3420e8b905da20c8f69a4379f5ef610901
davidjwhitfield <davidjwhitfield@gmail.com>
2025-09-15 13:41:32 +0100

    Implemented lazy loading in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import AppTheme from "./shared-theme/AppTheme";
@@ -8,22 +9,22 @@ import AppAppBar from "./components/AppAppBar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
-import Features from "./pages/Features";
-import Testimonials from "./pages/Testimonials";
-import Highlights from "./pages/Highlights";
-import Pricing from "./pages/Pricing";
-import FAQ from "./pages/FAQ";
-import Blog from "./pages/Blog";
-import About from "./pages/About";
-import Careers from "./pages/Careers";
-import Press from "./pages/Press";
-import Terms from "./pages/Terms";
-import Privacy from "./pages/Privacy";
-import Contact from "./pages/Contact";
-import PostDetail from "./pages/PostDetail";
-import EditPost from "./pages/EditPost";
-import ProtectedRoute from "./context/ProtectedRoute";
-import CreatePost from "./pages/CreatePost";
+const Features = lazy(() => import("./pages/Features"));
+const Testimonials = lazy(() => import("./pages/Testimonials"));
+const Highlights = lazy(() => import("./pages/Highlights"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const FAQ = lazy(() => import("./pages/FAQ"));
+const Blog = lazy(() => import("./pages/Blog"));
+const About = lazy(() => import("./pages/About"));
+const Careers = lazy(() => import("./pages/Careers"));
+const Press = lazy(() => import("./pages/Press"));
+const Terms = lazy(() => import("./pages/Terms"));
+const Privacy = lazy(() => import("./pages/Privacy"));
+const Contact = lazy(() => import("./pages/Contact"));
+const PostDetail = lazy(() => import("./pages/PostDetail"));
+const EditPost = lazy(() => import("./pages/EditPost"));
+const ProtectedRoute = lazy(() => import("./context/ProtectedRoute"));
+const CreatePost = lazy(() => import("./pages/CreatePost"));
 
 export default function App(props: { disableCustomTheme?: boolean }) {
   return (
@@ -36,45 +37,47 @@ export default function App(props: { disableCustomTheme?: boolean }) {
         component="main"
         sx={{ display: "flex", flexDirection: "column", my: 16, gap: 4 }}
       >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/features" element={<Features />} />
-          <Route path="/testimonials" element={<Testimonials />} />
-          <Route path="/highlights" element={<Highlights />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route
-            path="/blog"
-            element={
-              <ProtectedRoute>
-                <Blog />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/posts/:id" element={<PostDetail />} />
-          <Route
-            path="/edit-post/:id"
-            element={
-              <ProtectedRoute>
-                <EditPost />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/create-post"
-            element={
-              <ProtectedRoute>
-                <CreatePost />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/about" element={<About />} />
-          <Route path="/careers" element={<Careers />} />
-          <Route path="/press" element={<Press />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+            <Route path="/highlights" element={<Highlights />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route
+              path="/blog"
+              element={
+                <ProtectedRoute>
+                  <Blog />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route
+              path="/edit-post/:id"
+              element={
+                <ProtectedRoute>
+                  <EditPost />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/create-post"
+              element={
+                <ProtectedRoute>
+                  <CreatePost />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/about" element={<About />} />
+            <Route path="/careers" element={<Careers />} />
+            <Route path="/press" element={<Press />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
       </Container>
       <Footer />
     </AppTheme>
